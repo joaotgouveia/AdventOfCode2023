138a32d61937b6724d7aad4b146f4b9d01bc2d48
Joao Talone Gouveia <jjtucki4@gmail.com>
2023-12-20 16:16:25 +0000

    Finish part 1

--- day14/day14p1.rs
+++ day14/day14p1.rs
@@ -9,19 +9,18 @@ fn move_north(dish: &mut Vec<Vec<char>>, point: (usize, usize)) {
     while new_point.0 > 0 && dish[new_point.0 - 1][new_point.1] == '.' {
         new_point.0 -= 1;
     }
-    
+
     dish[point.0][point.1] = '.';
     dish[new_point.0][new_point.1] = 'O';
-
 }
 
 fn main() {
     let document = fs::read_to_string("day14.in").unwrap();
-    let dish: Vec<Vec<char>> = document.lines().map(|x| x.chars()).collect();
-    
-    let line_len = maps[0].len();
-    let line_count = maps.len();
-    
+    let mut dish: Vec<Vec<char>> = document.lines().map(|x| x.chars().collect()).collect();
+
+    let line_len = dish[0].len();
+    let line_count = dish.len();
+
     for i in 0..line_count {
         for j in 0..line_len {
             move_north(&mut dish, (i, j));
@@ -30,7 +29,7 @@ fn main() {
 
     let mut load = 0;
     for i in 0..line_count {
-        load += dish[i].iter().filter(|x| x == 'O').count() * (line_count - i);
+        load += dish[i].iter().filter(|&x| *x == 'O').count() * (line_count - i);
     }
 
     println!("{}", load);
