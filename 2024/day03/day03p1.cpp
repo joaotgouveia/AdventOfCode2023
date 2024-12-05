841aaf86026bfefebe1f0db4b672b5642eb1fd3a
João Gouveia <jtalonegouveia@gmail.com>
2024-12-05 14:47:24 +0000

    Ignore exceptions

--- 2024/day03/day03p1.cpp
+++ 2024/day03/day03p1.cpp
@@ -5,23 +5,19 @@
 #include <string>
 
 int main() {
-    try {
-        std::ifstream input("day03/day03.in");
-        std::string line;
-        int result = 0;
-        const std::regex regex(R"(mul\((\d+?),(\d+?)\))",
-                               std::regex_constants::optimize);
+    std::ifstream input("day03/day03.in");
+    std::string line;
+    int result = 0;
+    const std::regex regex(R"(mul\((\d+?),(\d+?)\))",
+                           std::regex_constants::optimize);
 
-        while (std::getline(input, line).good()) {
-            const auto end = std::sregex_iterator();
-            for (auto i = std::sregex_iterator(line.begin(), line.end(), regex);
-                 i != end; ++i) {
-                const std::smatch& mul = *i;
-                result += std::stoi(mul[1]) * std::stoi(mul[2]);
-            }
+    while (std::getline(input, line).good()) {
+        const auto end = std::sregex_iterator();
+        for (auto i = std::sregex_iterator(line.begin(), line.end(), regex);
+             i != end; ++i) {
+            const std::smatch& mul = *i;
+            result += std::stoi(mul[1]) * std::stoi(mul[2]);
         }
-        std::cout << result << "\n";
-    } catch (const std::regex_error& e) {
-        std::cout << "Regex failed: " << e.what() << "\n";
     }
+    std::cout << result << "\n";
 }
